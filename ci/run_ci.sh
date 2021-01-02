53e421b7a5a2dd2b2000c2f2c6abe2a98edc6013
b3m2a1 <b3m2a1@gmail.com>
2021-01-02 15:11:33 -0500

    Still trying to push

--- ci/run_ci.sh
+++ ci/run_ci.sh
@@ -17,9 +17,10 @@ cd /home
 PYTHONPATH=/home python3 Peeves/ci/tests/run_tests.py -v -d
 
 cd Peeves
-git checkout -B gh-pages
+git checkout gh-pages
 cd ..
 ## write some artifacts, maybe, in the future (e.g. generated data from data_gen tests or images from matplotlib)
 PYTHONPATH=/home python3 Peeves/ci/build_docs.py
+cd Peeves
 git add -A && git commit -m "Built out site"
 git push -u origin HEAD
