798749a012aa5eb6876023b4aa7c0d423c0ff122
b3m2a1 <b3m2a1@gmail.com>
2021-01-02 16:30:17 -0500

    Update run_ci.sh

--- ci/run_ci.sh
+++ ci/run_ci.sh
@@ -16,9 +16,7 @@ PYTHONPATH=/home python3 Peeves/ci/tests/run_tests.py -v -d
 # build docs and push
 PYTHONPATH=/home python3 Peeves/ci/build_docs.py
 cd Peeves
-git config --global user.name ${GITHUB_ACTOR}
-git config --global user.email ${GITHUB_ACTOR}@users.noreply.github.com
+git config user.name ${GITHUB_ACTOR}
+git config user.email ${GITHUB_ACTOR}@users.noreply.github.com
 git add -A && git commit -m "Built out docs"
-# wtf is going on
-git config --get remote.origin.url
 git push
