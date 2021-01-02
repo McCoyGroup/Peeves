05dd04fa93da1b22a1203004619cc402bcc5b054
b3m2a1 <b3m2a1@gmail.com>
2021-01-02 17:10:45 -0500

    wat

--- ci/run_ci.sh
+++ ci/run_ci.sh
@@ -7,6 +7,8 @@ set -e
 
 # get into the parent folder and merge in changes from the master branch
 cd /home/Peeves
+git config user.name ${GITHUB_ACTOR}
+git config user.email ${GITHUB_ACTOR}@users.noreply.github.com
 git checkout gh-pages
 git merge master
 ## run the test script
@@ -16,7 +18,5 @@ PYTHONPATH=/home python3 Peeves/ci/tests/run_tests.py -v -d
 # build docs and push
 PYTHONPATH=/home python3 Peeves/ci/build_docs.py
 cd Peeves
-git config user.name ${GITHUB_ACTOR}
-git config user.email ${GITHUB_ACTOR}@users.noreply.github.com
 git add -A && git commit -m "Built out docs"
 git push -u "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/McCoyGroup/Peeves.git" gh-pages
