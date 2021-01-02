d3ddf45f280a7f3470811aef39eb2f146365bfbb
b3m2a1 <b3m2a1@gmail.com>
2021-01-02 16:57:31 -0500

    Update run_ci.sh

--- ci/run_ci.sh
+++ ci/run_ci.sh
@@ -19,4 +19,5 @@ cd Peeves
 git config user.name ${GITHUB_ACTOR}
 git config user.email ${GITHUB_ACTOR}@users.noreply.github.com
 git add -A && git commit -m "Built out docs"
+git branch --show-current
 git push -u "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/McCoyGroup/Peeves.git" gh-pages
