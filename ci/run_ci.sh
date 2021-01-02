8531a8a51cc7f6e78014f5c6477c834a4e359bef
b3m2a1 <b3m2a1@gmail.com>
2021-01-02 16:01:21 -0500

    can't figure GH actions out ;_;

--- ci/run_ci.sh
+++ ci/run_ci.sh
@@ -5,22 +5,13 @@ set -e
 ## Runs CI
 ##
 
-# get into the parent folder
-cd /home
-# configure git in case we want to push stuff in the future
-git config --global user.name ${GITHUB_ACTOR}
-git config --global user.email ${GITHUB_ACTOR}@users.noreply.github.com
-# clone in Peeves for Unit testing
-#git clone https://github.com/McCoyGroup/Peeves.git
-## run the testing script
+# get into the parent folder and merge in changes from the master branch
+# since we're running on the gh-pages branch
+cd /home/Peeves
+git merge master
+## run the test script
 cd /home
 PYTHONPATH=/home python3 Peeves/ci/tests/run_tests.py -v -d
-
-# switch to the gh-pages branch and merge in changes from master
-cd Peeves
-git checkout gh-pages
-git merge master
-cd ..
 
 # build docs and push
 PYTHONPATH=/home python3 Peeves/ci/build_docs.py
@@ -28,4 +19,4 @@ cd Peeves
 git config --global user.name ${GITHUB_ACTOR}
 git config --global user.email ${GITHUB_ACTOR}@users.noreply.github.com
 git add -A && git commit -m "Built out docs"
-git push "https://github.com/McCoyGroup/Peeves.git" gh-pages
+git push origin gh-pages
